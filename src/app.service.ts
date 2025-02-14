58f9bcd275456a3982b1fb760bb8f599c04bb27f
haseebrehmat <haseeb.ali.dev@gmail.com>
2025-02-15 03:50:02 +0500

    Implement AppService to manage superheroes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { Superhero } from './models/superhero.model';
+import { CreateSuperheroDto } from './dto/create-superhero.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private superheroes: Superhero[] = [];
+
+  addSuperhero(createSuperheroDto: CreateSuperheroDto): Superhero {
+    // The validation is now handled by the DTO decorators
+    const superhero: Superhero = {
+      name: createSuperheroDto.name,
+      superpower: createSuperheroDto.superpower,
+      humilityScore: createSuperheroDto.humilityScore
+    };
+    
+    this.superheroes.push(superhero);
+    return superhero;
+  }
+
+  getSuperheroes(): Superhero[] {
+    return [...this.superheroes].sort((a, b) => b.humilityScore - a.humilityScore);
   }
 }
